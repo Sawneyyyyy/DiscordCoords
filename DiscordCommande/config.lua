9a42ba0f7a123e84e82da3388e5581e7b7c6eeb7
Sawney-Lua <75329542+Sawney-Lua@users.noreply.github.com>
2021-07-21 14:17:03 +0200

    Update config.lua

--- DiscordCommande/config.lua
+++ DiscordCommande/config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
 
-Config.Wbk = "https://discord.com/api/webhooks/867376784650797077/j1sdXIAQDnXEVjb-OgCBgBiHu2kI-v_Wnbsq5UMoXnpW2MJEcEXtv22mrvvSkzVE8flO" --Votre Weebhook
+Config.Wbk = "" --Votre Weebhook
